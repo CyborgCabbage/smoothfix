ee38214340580da710a43fa5e8c53213f97c14d1
CyborgCabbage <finlaylaidlaw9@gmail.com>
2023-01-19 22:17:20 +0000

    Finish mod

---
+++ src/main/java/cyborgcabbage/smoothfix/RenderBlockCache.java
@@ -0,0 +1,48 @@
+package cyborgcabbage.smoothfix;
+
+import net.minecraft.src.Block;
+import net.minecraft.src.IBlockAccess;
+
+import java.util.Arrays;
+
+final public class RenderBlockCache {
+    private static final int CACHE_RANGE = 1;
+    private static final int CACHE_SIZE = CACHE_RANGE*2+1;
+    private final boolean[] brightnessCached = new boolean[CACHE_SIZE*CACHE_SIZE*CACHE_SIZE];
+    private final float[] brightnessValue = new float[CACHE_SIZE*CACHE_SIZE*CACHE_SIZE];
+    private final boolean[] opacityCached = new boolean[CACHE_SIZE*CACHE_SIZE*CACHE_SIZE];
+    private final boolean[] opacityValue = new boolean[CACHE_SIZE*CACHE_SIZE*CACHE_SIZE];
+    private int offsetX = Integer.MAX_VALUE;
+    private int offsetY = Integer.MAX_VALUE;
+    private int offsetZ = Integer.MAX_VALUE;
+    private Block block;
+    private IBlockAccess access;
+
+    public void setupCache(Block block, IBlockAccess access, int x, int y, int z){
+        if(x != offsetX || y != offsetY || z != offsetZ || this.block != block || this.access != access) {
+            this.block = block;
+            this.access = access;
+            Arrays.fill(brightnessCached, false);
+            Arrays.fill(opacityCached, false);
+            offsetX = x;
+            offsetY = y;
+            offsetZ = z;
+        }
+    }
+    public float getBrightness(int relX, int relY, int relZ) {
+        int index = (relX+CACHE_RANGE)*CACHE_SIZE*CACHE_SIZE+(relY+CACHE_RANGE)*CACHE_SIZE+(relZ+CACHE_RANGE);
+        if (!brightnessCached[index]) {
+            brightnessValue[index] = block.getBlockBrightness(access, relX + offsetX, relY + offsetY, relZ + offsetZ);
+            brightnessCached[index] = true;
+        }
+        return brightnessValue[index];
+    }
+    public boolean getOpacity(int relX, int relY, int relZ) {
+        int index = (relX+CACHE_RANGE)*CACHE_SIZE*CACHE_SIZE+(relY+CACHE_RANGE)*CACHE_SIZE+(relZ+CACHE_RANGE);
+        if(!opacityCached[index]){
+            opacityValue[index] = Block.opaqueCubeLookup[access.getBlockId(relX+ offsetX, relY+ offsetY, relZ+ offsetZ)];
+            opacityCached[index] = true;
+        }
+        return opacityValue[index];
+    }
+}
--- src/main/java/cyborgcabbage/smoothfix/mixin/RenderBlocksMixin.java
+++ src/main/java/cyborgcabbage/smoothfix/mixin/RenderBlocksMixin.java
@@ -1,7 +1,9 @@
 package cyborgcabbage.smoothfix.mixin;
 
+import cyborgcabbage.smoothfix.RenderBlockCache;
 import net.minecraft.src.Block;
 import net.minecraft.src.IBlockAccess;
+import net.minecraft.src.MathHelper;
 import net.minecraft.src.RenderBlocks;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
@@ -14,7 +16,6 @@ public abstract class RenderBlocksMixin {
     @Shadow private boolean renderAllFaces = false;
     @Shadow public static boolean fancyGrass = true;
     @Shadow private boolean enableAO;
-    @Shadow private float lightValueOwn;
     @Shadow private int field_22352_G = 1;
     @Shadow private float colorRedTopLeft;
     @Shadow private float colorRedBottomLeft;
@@ -37,81 +38,57 @@ public abstract class RenderBlocksMixin {
     @Shadow public abstract void renderWestFace(Block block, double d, double d1, double d2, int i);
     @Shadow public abstract void renderSouthFace(Block block, double d, double d1, double d2, int i);
 
+    RenderBlockCache cache = new RenderBlockCache();
     /**
      * @author CyborgCabbage
      * @reason Some men just want to watch the world burn
      */
     @Overwrite
-
     public boolean renderStandardBlockWithAmbientOcclusion(Block block, int x, int y, int z, float x1, float y1, float z1) {
         this.enableAO = true;
-        this.lightValueOwn = block.getBlockBrightness(this.blockAccess, x, y, z);
-        float nooB = block.getBlockBrightness(this.blockAccess, x - 1, y, z);
-        float onoB = block.getBlockBrightness(this.blockAccess, x, y - 1, z);
-        float oonB = block.getBlockBrightness(this.blockAccess, x, y, z - 1);
-        float pooB = block.getBlockBrightness(this.blockAccess, x + 1, y, z);
-        float opoB = block.getBlockBrightness(this.blockAccess, x, y + 1, z);
-        float oopB = block.getBlockBrightness(this.blockAccess, x, y, z + 1);
-        boolean ppo = !Block.opaqueCubeLookup[this.blockAccess.getBlockId(x + 1, y + 1, z)];
-        boolean pno = !Block.opaqueCubeLookup[this.blockAccess.getBlockId(x + 1, y - 1, z)];
-        boolean pop = !Block.opaqueCubeLookup[this.blockAccess.getBlockId(x + 1, y, z + 1)];
-        boolean pon = !Block.opaqueCubeLookup[this.blockAccess.getBlockId(x + 1, y, z - 1)];
-        boolean npo = !Block.opaqueCubeLookup[this.blockAccess.getBlockId(x - 1, y + 1, z)];
-        boolean nno = !Block.opaqueCubeLookup[this.blockAccess.getBlockId(x - 1, y - 1, z)];
-        boolean non = !Block.opaqueCubeLookup[this.blockAccess.getBlockId(x - 1, y, z - 1)];
-        boolean nop = !Block.opaqueCubeLookup[this.blockAccess.getBlockId(x - 1, y, z + 1)];
-        boolean opp = !Block.opaqueCubeLookup[this.blockAccess.getBlockId(x, y + 1, z + 1)];
-        boolean opn = !Block.opaqueCubeLookup[this.blockAccess.getBlockId(x, y + 1, z - 1)];
-        boolean onp = !Block.opaqueCubeLookup[this.blockAccess.getBlockId(x, y - 1, z + 1)];
-        boolean onn = !Block.opaqueCubeLookup[this.blockAccess.getBlockId(x, y - 1, z - 1)];
+        cache.setupCache(block, blockAccess, x, y, z);
         boolean notGrass = block != Block.grass;
-        boolean somethingRendered = renderSide(block, x,y,z,x1,y1,z1,notGrass,0,
-                0, -1, 0, onoB,
-                0, 0, 1, onp, onn,
-                -1, 0, 0, nno, pno
+        boolean somethingRendered = renderSide(block, x, y, z, x1, y1, z1, notGrass,0,
+                0, -1, 0, (float)block.minY,
+                0, 0, 1, (float)block.maxZ, (float)block.minZ,
+                -1, 0, 0, 1-(float)block.minX, 1-(float)block.maxX
         );
-        somethingRendered |= renderSide(block, x,y,z,x1,y1,z1,notGrass,1,
-                0,1,0, opoB,
-                0,0,1, opp, opn,
-                1,0,0, ppo, npo
+        somethingRendered |= renderSide(block, x, y, z, x1, y1, z1, notGrass,1,
+                0,1,0, 1-(float)block.maxY,
+                0,0,1, (float)block.maxZ, (float)block.minZ,
+                1,0,0, (float)block.maxX, (float)block.minX
         );
-        somethingRendered |= renderSide(block, x,y,z,x1,y1,z1,notGrass,2,
-                0,0,-1, oonB,
-                -1,0,0, non, pon,
-                0,1,0, opn, onn
+        somethingRendered |= renderSide(block, x, y, z, x1, y1, z1, notGrass,2,
+                0,0,-1, (float)block.minZ,
+                -1,0,0, 1-(float)block.minX, 1-(float)block.maxX,
+                0,1,0, (float)block.maxY, (float)block.minY
         );
-        somethingRendered |= renderSide(block, x,y,z,x1,y1,z1,notGrass,3,
-                0,0,1, oopB,
-                0,1,0, opp, onp,
-                -1,0,0, nop, pop
+        somethingRendered |= renderSide(block, x, y, z, x1, y1, z1, notGrass,3,
+                0,0,1, 1-(float)block.maxZ,
+                0,1,0, (float)block.maxY, (float)block.minY,
+                -1,0,0, 1-(float)block.minX, 1-(float)block.maxX
         );
-        somethingRendered |= renderSide(block, x,y,z,x1,y1,z1,notGrass,4,
-                -1,0,0, nooB,
-                0,0,1, nop, non,
-                0,1,0, npo, nno
+        somethingRendered |= renderSide(block, x, y, z, x1, y1, z1, notGrass,4,
+                -1,0,0, (float)block.minX,
+                0,0,1, (float)block.maxZ, (float)block.minZ,
+                0,1,0, (float)block.maxY, (float)block.minY
         );
-        somethingRendered |= renderSide(block, x,y,z,x1,y1,z1,notGrass,5,
-                1,0,0, pooB,
-                0,0,1, pop, pon,
-                0,-1,0, pno, ppo
+        somethingRendered |= renderSide(block, x, y, z, x1, y1, z1, notGrass,5,
+                1,0,0, 1-(float)block.maxX,
+                0,0,1, (float)block.maxZ, (float)block.minZ,
+                0,-1,0, 1-(float)block.minY, 1-(float)block.maxY
         );
         this.enableAO = false;
         return somethingRendered;
     }
 
-    private static final float[] SIDE_LIGHT_MULTIPLIER = {0.5f, 1.f, 0.7f, 0.7f, 0.7f, 0.7f};
+    private static final float[] SIDE_LIGHT_MULTIPLIER = {0.5f, 1.f, 0.8f, 0.8f, 0.6f, 0.6f};
 
-    boolean renderSide(Block block, int x, int y, int z, float x1, float y1, float z1, boolean notGrass, int side,
-                int dirX, int dirY, int dirZ, float dirB,
-                int topX, int topY, int topZ, boolean topT, boolean botT,
-                int lefX, int lefY, int lefZ, boolean lefT, boolean rigT
+    final boolean renderSide(Block block, int x, int y, int z, float x1, float y1, float z1, boolean notGrass, int side,
+                int dirX, int dirY, int dirZ, float depth,
+                int topX, int topY, int topZ, float topP, float botP,
+                int lefX, int lefY, int lefZ, float lefP, float rigP
     ){
-        int botX = -topX;
-        int botY = -topY;
-        int botZ = -topZ;
-        int rigX = -lefX;
-        int rigY = -lefY;
-        int rigZ = -lefZ;
         boolean rendered = false;
         boolean flag = (side == 1) || notGrass;
         if (this.renderAllFaces || block.shouldSideBeRendered(this.blockAccess, x+dirX, y+dirY, z+dirZ, side)) {
@@ -119,64 +96,79 @@ public abstract class RenderBlocksMixin {
             float lightBR;
             float lightBL;
             float lightTL;
+           
             if (this.overbright) {
                 lightTR = 1.0F;
                 lightBR = 1.0F;
                 lightBL = 1.0F;
                 lightTL = 1.0F;
             } else if (this.field_22352_G <= 0) {
-                lightTL = lightBL = lightBR = lightTR = dirB;
+                lightTL = lightBL = lightBR = lightTR = cache.getBrightness(dirX, dirY, dirZ);
             } else {
-                float lB = block.getBlockBrightness(this.blockAccess, x+dirX+lefX, y+dirY+lefY, z+dirZ+lefZ);
-                float bB = block.getBlockBrightness(this.blockAccess, x+dirX+botX, y+dirY+botY, z+dirZ+botZ);
-                float tB = block.getBlockBrightness(this.blockAccess, x+dirX+topX, y+dirY+topY, z+dirZ+topZ);
-                float rB = block.getBlockBrightness(this.blockAccess, x+dirX+rigX, y+dirY+rigY, z+dirZ+rigZ);
-                float blB;
-                if (!botT && !lefT) {//Both opaque
-                    blB = lB;
-                } else {
-                    blB = block.getBlockBrightness(this.blockAccess, x+dirX+lefX+botX, y+dirY+lefY+botY, z+dirZ+lefZ+botZ);
+                {
+                    float dirB = cache.getBrightness(dirX, dirY, dirZ);
+                    boolean lefT = cache.getOpacity(dirX + lefX, dirY + lefY, dirZ + lefZ);
+                    boolean botT = cache.getOpacity(dirX - topX, dirY - topY, dirZ - topZ);
+                    boolean topT = cache.getOpacity(dirX + topX, dirY + topY, dirZ + topZ);
+                    boolean rigT = cache.getOpacity(dirX - lefX, dirY - lefY, dirZ - lefZ);
+                    float lB = cache.getBrightness(dirX + lefX, dirY + lefY, dirZ + lefZ);
+                    float bB = cache.getBrightness(dirX - topX, dirY - topY, dirZ - topZ);
+                    float tB = cache.getBrightness(dirX + topX, dirY + topY, dirZ + topZ);
+                    float rB = cache.getBrightness(dirX - lefX, dirY - lefY, dirZ - lefZ);
+                    float blB = botT && lefT ? lB : cache.getBrightness(dirX + lefX - topX, dirY + lefY - topY, dirZ + lefZ - topZ);
+                    float tlB = topT && lefT ? lB : cache.getBrightness(dirX + lefX + topX, dirY + lefY + topY, dirZ + lefZ + topZ);
+                    float brB = botT && rigT ? rB : cache.getBrightness(dirX - lefX - topX, dirY - lefY - topY, dirZ - lefZ - topZ);
+                    float trB = topT && rigT ? rB : cache.getBrightness(dirX - lefX + topX, dirY - lefY + topY, dirZ - lefZ + topZ);
+                    lightTL = (tlB + lB + tB + dirB) / 4.0F;
+                    lightTR = (tB + dirB + trB + rB) / 4.0F;
+                    lightBR = (dirB + bB + rB + brB) / 4.0F;
+                    lightBL = (lB + blB + dirB + bB) / 4.0F;
                 }
-                float tlB;
-                if (!topT && !lefT) {
-                    tlB = lB;
-                } else {
-                    tlB = block.getBlockBrightness(this.blockAccess, x+dirX+lefX+topX, y+dirY+lefY+topY, z+dirZ+lefZ+topZ);
+                if(depth > 0.01){
+                    float dirB = cache.getBrightness(0, 0, 0);
+                    boolean lefT = cache.getOpacity(lefX, lefY, lefZ);
+                    boolean botT = cache.getOpacity(-topX, -topY, -topZ);
+                    boolean topT = cache.getOpacity(topX, topY, topZ);
+                    boolean rigT = cache.getOpacity(-lefX, -lefY, -lefZ);
+                    float lB = cache.getBrightness(lefX, lefY, lefZ);
+                    float bB = cache.getBrightness(-topX, -topY, -topZ);
+                    float tB = cache.getBrightness(topX, topY, topZ);
+                    float rB = cache.getBrightness(-lefX, -lefY, -lefZ);
+                    float blB = botT && lefT ? lB : cache.getBrightness(lefX - topX, lefY - topY, lefZ - topZ);
+                    float tlB = topT && lefT ? lB : cache.getBrightness(lefX + topX, lefY + topY, lefZ + topZ);
+                    float brB = botT && rigT ? rB : cache.getBrightness(-lefX - topX, -lefY - topY, -lefZ - topZ);
+                    float trB = topT && rigT ? rB : cache.getBrightness(-lefX + topX, -lefY + topY, -lefZ + topZ);
+                    lightTL = (tlB + lB + tB + dirB) / 4.0F * depth + lightTL*(1-depth);
+                    lightTR = (tB + dirB + trB + rB) / 4.0F * depth + lightTR*(1-depth);
+                    lightBR = (dirB + bB + rB + brB) / 4.0F * depth + lightBR*(1-depth);
+                    lightBL = (lB + blB + dirB + bB) / 4.0F * depth + lightBL*(1-depth);
                 }
-                float brB;
-                if (!botT && !rigT) {
-                    brB = rB;
-                } else {
-                    brB = block.getBlockBrightness(this.blockAccess, x+dirX+rigX+botX, y+dirY+rigY+botY, z+dirZ+rigZ+botZ);
-                }
-                float trB;
-                if (!topT && !rigT) {
-                    trB = rB;
-                } else {
-                    trB = block.getBlockBrightness(this.blockAccess, x+dirX+rigX+topX, y+dirY+rigY+topY, z+dirZ+rigZ+topZ);
-                }
-
-                lightTL = (tlB + lB + tB + dirB) / 4.0F;
-                lightTR = (tB + dirB + trB + rB) / 4.0F;
-                lightBR = (dirB + bB + rB + brB) / 4.0F;
-                lightBL = (lB + blB + dirB + bB) / 4.0F;
             }
 
             this.colorRedTopLeft = this.colorRedBottomLeft = this.colorRedBottomRight = this.colorRedTopRight = (flag ? x1 : 1.0F) * SIDE_LIGHT_MULTIPLIER[side];
             this.colorGreenTopLeft = this.colorGreenBottomLeft = this.colorGreenBottomRight = this.colorGreenTopRight = (flag ? y1 : 1.0F) * SIDE_LIGHT_MULTIPLIER[side];
             this.colorBlueTopLeft = this.colorBlueBottomLeft = this.colorBlueBottomRight = this.colorBlueTopRight = (flag ? z1 : 1.0F) * SIDE_LIGHT_MULTIPLIER[side];
-            this.colorRedTopLeft *= lightTL;
-            this.colorGreenTopLeft *= lightTL;
-            this.colorBlueTopLeft *= lightTL;
-            this.colorRedBottomLeft *= lightBL;
-            this.colorGreenBottomLeft *= lightBL;
-            this.colorBlueBottomLeft *= lightBL;
-            this.colorRedBottomRight *= lightBR;
-            this.colorGreenBottomRight *= lightBR;
-            this.colorBlueBottomRight *= lightBR;
-            this.colorRedTopRight *= lightTR;
-            this.colorGreenTopRight *= lightTR;
-            this.colorBlueTopRight *= lightTR;
+            float tl = topP * lightTL + (1 - topP) * lightBL;
+            float tr = topP * lightTR + (1 - topP) * lightBR;
+            float bl = botP * lightTL + (1 - botP) * lightBL;
+            float br = botP * lightTR + (1 - botP) * lightBR;
+            float ltl = lefP * tl + (1 - lefP) * tr;
+            float lbl = lefP * bl + (1 - lefP) * br;
+            float lbr = rigP * bl + (1 - rigP) * br;
+            float ltr = rigP * tl + (1 - rigP) * tr;
+            this.colorRedTopLeft *= ltl;
+            this.colorGreenTopLeft *= ltl;
+            this.colorBlueTopLeft *= ltl;
+            this.colorRedBottomLeft *= lbl;
+            this.colorGreenBottomLeft *= lbl;
+            this.colorBlueBottomLeft *= lbl;
+            this.colorRedBottomRight *= lbr;
+            this.colorGreenBottomRight *= lbr;
+            this.colorBlueBottomRight *= lbr;
+            this.colorRedTopRight *= ltr;
+            this.colorGreenTopRight *= ltr;
+            this.colorBlueTopRight *= ltr;
+
             int tex;
             if (this.overbright) {
                 tex = block.getBlockOverbright(this.blockAccess, x, y, z, side);
